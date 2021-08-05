7654948e65d81d182efcf109512533546c5c525b
planchapin <ferraccithomas@gmail.com>
2021-08-05 14:58:44 +0200

    projet de gestion pour les users

---
+++ src/com/inti/entities/Utilisatreur.java
@@ -0,0 +1,31 @@
+package com.inti.entities;
+
+public class Utilisatreur {
+
+	private String nom;
+	private String prenom;
+	private int age;
+	public Utilisatreur() {
+		super();
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
